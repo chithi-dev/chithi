1a73b24aba274c4e9d0f8d408ccc638b8bdecb2c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-12-23 19:06:16 +0600

    add

--- src/backend/app/models/config.py
+++ src/backend/app/models/config.py
@@ -3,7 +3,7 @@ from sqlalchemy import text
 from uuid import UUID
 from datetime import datetime
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy import Integer, String, Date
+from sqlalchemy import Integer, String, DateTime, BigInteger
 from datetime import timedelta
 from app.converter.bytes import ByteSize
 
@@ -20,19 +20,29 @@ class Config(SQLModel, table=True):
     )
 
     # Max limits
-    total_storage_limit: int = ByteSize(gb=10).total_bytes()  # 10gb
-    max_file_size_limit: int = ByteSize(mb=100).total_bytes()  # 100mb
+    total_storage_limit: int = Field(
+        default=ByteSize(gb=10).total_bytes(), sa_column=Column(BigInteger)
+    )
+    max_file_size_limit: int = Field(
+        default=ByteSize(mb=100).total_bytes(), sa_column=Column(BigInteger)
+    )
 
     # Default constraints
     default_expiry: int = int(timedelta(days=7).total_seconds())
-    default_number_of_downloads: int = 5
+    default_number_of_downloads: int = 10
 
     # Markdown fields
     site_description: str = "Welcome to Chithi"
 
     # Customizable fields
-    download_configs: list[int] = Field(default=[], sa_column=Column(ARRAY(Integer)))
-    time_configs: list[datetime] = Field(default=[], sa_column=Column(ARRAY(Date)))
+    download_configs: list[int] = Field(
+        default=[10],
+        sa_column=Column(ARRAY(Integer)),
+    )
+    time_configs: list[datetime] = Field(
+        default=[int(timedelta(days=7).total_seconds())],
+        sa_column=Column(ARRAY(DateTime)),
+    )
 
     allowed_file_types: list[str] = Field(default=[], sa_column=Column(ARRAY(String)))
     banned_file_types: list[str] = Field(default=[], sa_column=Column(ARRAY(String)))
