7e7a9cadd7fa8677d3e18a0ec15422bf254c4cfc
basplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-12-29 20:37:46 +0600

    Update +layout.ts

--- src/frontend/src/routes/(web_crypto)/+layout.ts
+++ src/frontend/src/routes/(web_crypto)/+layout.ts
@@ -1,12 +1,15 @@
 import { error } from '@sveltejs/kit';
 import { is_web_crypto_available } from '$lib/functions/compatibility';
 import type { LayoutLoad } from '../$types';
+import { browser } from '$app/environment';
 
 export const load: LayoutLoad = (event) => {
-	if (true) {
-		error(500, {
-			message: 'WebCrypto API is not available',
-			code: 'WEBCRYPTO_UNAVAILABLE'
-		});
+	if (browser) {
+		if (!is_web_crypto_available()) {
+			error(500, {
+				message: 'WebCrypto API is not available',
+				code: 'WEBCRYPTO_UNAVAILABLE'
+			});
+		}
 	}
 };
