17ec46a8e5405630f4127839fa39ae82dd5cc371
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-01-04 17:19:49 +0600

    feat: docker fixes

--- src/frontend/src/lib/consts/backend.ts
+++ src/frontend/src/lib/consts/backend.ts
@@ -1,4 +1,6 @@
-export const BACKEND_API = 'http://localhost:8000';
+import { env } from '$env/dynamic/public';
+
+export const BACKEND_API = env.PUBLIC_BACKEND_API ?? 'http://localhost:8000';
 export const LOGIN_URL = `${BACKEND_API}/login`;
 export const USER_URL = `${BACKEND_API}/user`;
 export const CONFIG_URL = `${BACKEND_API}/config`;
