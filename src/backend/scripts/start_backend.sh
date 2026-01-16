2642f47919704962944301c2ef8a0030a26487a8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-01-16 22:44:54 +0600

    Update start_backend.sh

--- src/backend/scripts/start_backend.sh
+++ src/backend/scripts/start_backend.sh
@@ -3,5 +3,11 @@ set -e
 
 cd "$(dirname "$0")/.." || exit 1
 
+UVICORN_ARGS=()
+
+if [ -n "$ROOT_PATH" ]; then
+    UVICORN_ARGS+=("--root-path" "$ROOT_PATH")
+fi
+
 alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 "${UVICORN_ARGS[@]}"
