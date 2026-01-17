3d5c1dbcf473cd704eae34828690d3293250f221
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-01-17 16:05:25 +0600

    feat: enable sourcemap

--- src/frontend/svelte.config.ts
+++ src/frontend/svelte.config.ts
@@ -12,7 +12,9 @@ export default {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	build: {
+		sourcemap: true
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-
+	
 	test: {
 		expect: { requireAssertions: true },
 
