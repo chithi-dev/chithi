21a7964d55eae008093b377a36f18a20febb2d83
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-01-21 00:13:13 +0600

    Update start_backend.sh

--- src/backend/scripts/start_backend.sh
+++ src/backend/scripts/start_backend.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")/.." || exit 1
 
 alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --timeout-keep-alive 0
