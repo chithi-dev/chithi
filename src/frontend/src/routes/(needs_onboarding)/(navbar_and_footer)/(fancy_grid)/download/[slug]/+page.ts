e49870d4b39bcb78b514913a37fb5f7169b3b0e1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-01-25 16:49:54 +0600

    feat: fix legacy parsing

--- src/frontend/src/routes/(needs_onboarding)/(navbar_and_footer)/(fancy_grid)/download/[slug]/+page.ts
+++ src/frontend/src/routes/(needs_onboarding)/(navbar_and_footer)/(fancy_grid)/download/[slug]/+page.ts
@@ -2,9 +2,12 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ url }) => {
-	const key = url.searchParams.get('secret');
+	// Prefer key from URL fragment (hash), fallback to legacy ?secret= query param
+	const hash = url.hash ? url.hash.slice(1) : null;
+	const key = hash || url.searchParams.get('secret');
+	/*	const key = url.hash ? url.hash.slice(1) : null;*/ // Remove this to always use hash for key
 	if (!key) {
-		error(400, {
+		throw error(400, {
 			code: 'MISSING_KEY',
 			message:
 				'The URL is missing the decryption key required to access this file. Please ensure you have the complete link.'
