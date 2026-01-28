c9be8658595276c981b8ac5c1bb89456e6e14bdd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-01-28 19:49:53 +0600

    Update markdown.ts

--- src/frontend/src/lib/functions/markdown.ts
+++ src/frontend/src/lib/functions/markdown.ts
@@ -4,12 +4,12 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
 
-export async function html_to_markdown(markdown: string) {
-	const result = await unified()
+export function html_to_markdown(markdown: string) {
+	const result = unified()
 		.use(remarkParse)
 		.use(remarkRehype) // Markdown → HTML AST
 		.use(rehypeSanitize) // Sanitize HTML AST
 		.use(rehypeStringify) // HTML AST → string
-		.process(markdown);
+		.processSync(markdown);
 	return result.toString();
 }
