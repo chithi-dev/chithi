398682f4997ad5e12f0c59f2309cca1e4dbfae7b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-05 18:26:58 +0600

    feat: fix `@zip.js` issue by removing SSR

--- src/frontend/src/routes/(needs_onboarding)/(navbar_and_footer)/+page.ts
+++ src/frontend/src/routes/(needs_onboarding)/(navbar_and_footer)/+page.ts
@@ -1,5 +1,5 @@
 import { prefetch } from '#queries/config';
-import type { PageLoad } from './(fancy_grid)/$types';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent, fetch }) => {
 	const { queryClient } = await parent();
--- src/frontend/src/routes/+layout.server.ts
+++ src/frontend/src/routes/+layout.server.ts
@@ -1,1 +1,3 @@
 export const trailingSlash = 'always';
+// @zip.js does not work in ssr mode without putting it in `dependencies`
+export const ssr = false;
