9ae7e36959ffa7dbde1e9d5333926f3f8f574236
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-06 11:00:05 +0600

    add

--- src/frontend/src/lib/markdown/markdown.ts
+++ src/frontend/src/lib/markdown/markdown.ts
@@ -3,13 +3,15 @@ import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
+import rehypeEmailMangle from './plugins/email-obfuscate';
 
-export function html_to_markdown(markdown: string) {
-	const result = unified()
+export async function html_to_markdown(markdown: string) {
+	const result = await unified()
 		.use(remarkParse)
 		.use(remarkRehype) // Markdown → HTML AST
+		.use(rehypeEmailMangle) // Obfuscate email addresses
 		.use(rehypeSanitize) // Sanitize HTML AST
 		.use(rehypeStringify) // HTML AST → string
-		.processSync(markdown);
+		.process(markdown);
 	return result.toString();
 }
