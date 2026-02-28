33cd1c7d43c3bf7cf006bb7f850ee0b48c0b6cc0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-28 12:09:19 +0600

    add

--- src/frontend/src/routes/(needs_onboarding)/login/+page.server.ts
+++ src/frontend/src/routes/(needs_onboarding)/login/+page.server.ts
@@ -5,18 +5,11 @@ import { schema } from './schema';
 
 export const actions = {
 	default: async ({ request }) => {
-		const data = await request.clone().formData();
-		console.log('Raw Form Data:', Object.fromEntries(data));
-
 		const form = await superValidate(request, zod4(schema));
-		console.log(request);
-		console.log(form);
 
 		if (!form.valid) {
 			return fail(400, { form });
 		}
-
-		// TODO: Do something with the validated form.data
 
 		// Return the form with a status message
 		return message(form, 'Form posted successfully!');
