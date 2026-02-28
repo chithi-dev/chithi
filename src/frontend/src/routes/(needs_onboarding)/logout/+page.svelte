499bd7ba0c7d207fcdae5605bc2f91926cae9c69
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-28 14:21:30 +0600

    Update +page.svelte

--- src/frontend/src/routes/(needs_onboarding)/logout/+page.svelte
+++ src/frontend/src/routes/(needs_onboarding)/logout/+page.svelte
@@ -1,9 +1,20 @@
 <script>
 	import { user_store } from '$lib/store/user.svelte';
+	import { page } from '$app/state';
+	import { onMount } from 'svelte';
 
 	const { unauthenticate } = user_store();
 
-	$effect.pre(() => {
+	const next = $derived.by(() => {
+		const url = page.url.searchParams.get('next') ?? '/';
+		if (url.startsWith('/admin')) {
+			return '/';
+		}
+		return url;
+	});
+
+	onMount(() => {
 		unauthenticate();
+		window.location.href = next;
 	});
 </script>
