45674e7cd28b01421a72f61354d6521d45c34a62
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-28 11:32:00 +0600

    add

--- src/frontend/src/routes/(needs_onboarding)/login/+page.ts
+++ src/frontend/src/routes/(needs_onboarding)/login/+page.ts
@@ -1,7 +1,15 @@
 import { definePageMetaTags } from 'svelte-meta-tags';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod/v3';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = () => {
+const schema = z.object({
+	email: z.string().min(1, 'Email or Username is required'),
+	password: z.string().min(1, 'Password is required')
+});
+
+export const load: PageLoad = async () => {
 	const pageTags = definePageMetaTags({
 		title: 'Login',
 		description: 'Login to your chithi instance.',
@@ -10,6 +18,7 @@ export const load: PageLoad = () => {
 			description: 'Login to your chithi instance.'
 		}
 	});
+	const form = await superValidate(zod(schema));
 
-	return { ...pageTags };
+	return { form, ...pageTags };
 };
