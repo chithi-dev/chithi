6ef4f62fac9c7613e9f83df9663602b81663bf3f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-03-13 07:11:25 +0600

    add

---
+++ src/frontend/src/routes/information/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	const repo = 'https://github.com/chithi-dev/chithi';
+
+	const version = __APP_VERSION__;
+	const commit = __COMMIT_SHA__;
+
+	const commitUrl = `${repo}/commit/${commit}`;
+</script>
+
+<footer>
+	{#if version.startsWith('v0.0.0')}
+		<a href={commitUrl} target="_blank">
+			{version}
+		</a>
+	{:else}
+		{version}
+	{/if}
+</footer>
