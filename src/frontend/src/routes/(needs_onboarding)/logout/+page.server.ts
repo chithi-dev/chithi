765584046be1757a562238c63ec0dd19f31eeac5
mhdgning131 <gningmoustapha078@gmail.com>
2026-04-01 02:23:32 +0000

    Implemented URL Validator for ?next= paramerter

--- src/frontend/src/routes/(needs_onboarding)/logout/+page.server.ts
+++ src/frontend/src/routes/(needs_onboarding)/logout/+page.server.ts
@@ -1,14 +1,13 @@
 import { logout } from '$lib/remote/auth.remote';
 import { redirect } from '@sveltejs/kit';
+import { validateRedirectUrl } from '$lib/utils';
 
 export const actions = {
 	default: async ({ url }) => {
 		await logout();
 
 		let next = url.searchParams.get('next') ?? '/';
-		if (next.startsWith('/admin')) {
-			next = '/';
-		}
+		next = validateRedirectUrl(next, url.origin);
 		throw redirect(303, next);
 	}
 };
