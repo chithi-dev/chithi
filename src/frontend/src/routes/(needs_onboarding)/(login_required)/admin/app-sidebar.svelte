f72ba29ba8022f2cdc425ec37d5be6a8903738f4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-04-03 18:06:57 +0600

    add

--- src/frontend/src/routes/(needs_onboarding)/(login_required)/admin/app-sidebar.svelte
+++ src/frontend/src/routes/(needs_onboarding)/(login_required)/admin/app-sidebar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index';
 	import { Settings, UserPen, Link } from '@lucide/svelte';
+	import { page } from '$app/state';
+
 	const items = [
 		{
 			title: 'Profile',
@@ -20,26 +22,36 @@
 	];
 </script>
 
-<Sidebar.Root>
+<Sidebar.Root variant="inset">
 	<Sidebar.Header>
 		<Sidebar.Menu>
 			<Sidebar.MenuItem>
-				<Sidebar.MenuButton class="data-[slot=sidebar-menu-button]:p-1.5!">
-					<a href="/">
-						<p class=" font-semibold">Chithi</p>
-					</a>
+				<Sidebar.MenuButton size="lg" class="data-[slot=sidebar-menu-button]:p-1.5">
+					{#snippet child({ props })}
+						<a href="/" {...props}>
+							<div
+								class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"
+							>
+								<p class="font-bold">C</p>
+							</div>
+							<div class="grid flex-1 text-left text-sm leading-tight">
+								<span class="truncate font-semibold">Chithi</span>
+								<span class="truncate text-xs">Admin Panel</span>
+							</div>
+						</a>
+					{/snippet}
 				</Sidebar.MenuButton>
 			</Sidebar.MenuItem>
 		</Sidebar.Menu>
 	</Sidebar.Header>
 	<Sidebar.Content>
 		<Sidebar.Group>
-			<Sidebar.GroupLabel>Chithi Admin Panel</Sidebar.GroupLabel>
+			<Sidebar.GroupLabel>Navigation</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
 					{#each items as item (item.title)}
 						<Sidebar.MenuItem>
-							<Sidebar.MenuButton>
+							<Sidebar.MenuButton isActive={page.url.pathname === item.url}>
 								{#snippet child({ props })}
 									<a href={item.url} {...props}>
 										<item.icon />
@@ -53,4 +65,5 @@
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
 	</Sidebar.Content>
+	<Sidebar.Rail />
 </Sidebar.Root>
