e2945830993a205a2df7b6b0d9b221af28f92bec
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-04-13 19:27:27 +0600

    use redis singleton

--- src/backend/app/guards/rate_limit.py
+++ src/backend/app/guards/rate_limit.py
@@ -4,12 +4,8 @@ from time import time_ns
 from fastapi_limiter.depends import RateLimiter
 from pyrate_limiter import BucketFactory, Limiter, Rate, RateItem
 from pyrate_limiter.buckets.redis_bucket import RedisBucket
-from redis.asyncio import ConnectionPool as AsyncConnectionPool, Redis as AsyncRedis
 
-from app.settings import settings
-
-pool = AsyncConnectionPool.from_url(settings.REDIS_ENDPOINT)
-redis_db = AsyncRedis(connection_pool=pool)
+from app.singletons.redis import RedisClient
 
 
 class RedisBucketFactory(BucketFactory):
@@ -23,7 +19,7 @@ class RedisBucketFactory(BucketFactory):
     async def get(self, item: RateItem) -> RedisBucket:
         bucket_key = f"rate_limit:{item.name}"
         if bucket_key not in self._buckets:
-            res = RedisBucket.init(self.rates, redis_db, bucket_key)
+            res = RedisBucket.init(self.rates, RedisClient.get(), bucket_key)
             if inspect.isawaitable(res):
                 res = await res
             self._buckets[bucket_key] = res
