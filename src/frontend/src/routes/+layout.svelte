87f17eb6778cea7abdf02e59cc5914ff03d8d9c3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-04-17 21:58:48 +0600

    migrate: fronts from local to @fontsource

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import '#css/fonts.scss';
+
 	import '#css/nprogress.scss';
 	import '#css/tailwind.css';
+
+	// Fonts import
+	import "@fontsource/geist";
+import "@fontsource/jetbrains-mono";
 
 	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import { page } from '$app/state';
