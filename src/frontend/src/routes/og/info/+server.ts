cf313e2b52d13159c422257073059f564a7729ec
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-05-19 10:51:25 +0600

    feat: update font name (#863)

--- src/frontend/src/routes/og/info/+server.ts
+++ src/frontend/src/routes/og/info/+server.ts
@@ -21,7 +21,7 @@ export async function GET({ url }: RequestEvent) {
 		stylesheets: [style],
 		fonts: [
 			{
-				name: 'Geist',
+				name: 'Geist Variable',
 				data: () => read(Geist).arrayBuffer()
 			}
 		]
