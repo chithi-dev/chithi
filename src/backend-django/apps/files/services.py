610c828648bf425f5723b8ea6e9bf7013c868075
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-07-27 17:24:29 +0600

    refactor: remove unnecessary region_name from S3 config

--- src/backend-django/apps/files/services.py
+++ src/backend-django/apps/files/services.py
@@ -10,7 +10,6 @@ def _get_s3_resource():
         endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name="us-east-1",
     )
 
 
@@ -20,7 +19,6 @@ def _get_s3_client():
         endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name="us-east-1",
     )
 
 
--- src/backend-django/apps/files/views.py
+++ src/backend-django/apps/files/views.py
@@ -18,7 +18,6 @@ def _get_s3_resource():
         endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name="us-east-1",
     )
 
 
