4f1848fcdfd4d6fb9026eaee52510e45ee4232b9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-07-31 11:59:59 +0600

    perf: enable esbuild minification in JS SDK build

--- sdks/js/scripts/build.ts
+++ sdks/js/scripts/build.ts
@@ -4,6 +4,7 @@ import wasmCompressPlugin from '../plugins/esbuild-plugin-wasm-compress.js';
 await esbuild.build({
     entryPoints: ['src/index.ts'],
     bundle: true,
+    minify: true,
     format: 'esm',
     platform: 'browser',
     outfile: 'dist/index.js',
